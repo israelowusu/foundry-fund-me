8f472d05f56a955c6ec3fa5abe7827926e7a9401
bratan <oaisrael633@gmail.com>
2024-09-15 22:22:28 +0100

    deploy mocks for anvil and return mock addresses

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -8,8 +8,9 @@
 pragma solidity ^0.8.17;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
-contract HelperConfig {
+contract HelperConfig is Script{
     // If we are on a local anvil, we deploy mocks
     // Otherwise, grab the existing address on the live network
     NetworkConfig public activeNetworkConfig;
@@ -32,7 +33,17 @@ contract HelperConfig {
         return sepoliaConfig;
     }
 
-    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {
+    function getAnvilEthConfig() public returns (NetworkConfig memory) {
         // price feed address
+
+        // 1. Deploy the mocks
+        // 2. Return the mock address
+
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(8, 2000e8);
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({priceFeed: address(mockPriceFeed)});
+        return anvilConfig;
     }
 }
